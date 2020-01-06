640db64f84e6ab901a659b2be1032e4b5e6e93ca
Kingshuk Bagchi <kbagchi@careindia.org>
2020-01-06 17:58:57 +0530

    DatePicker Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { DatePicker } from '@progress/kendo-dateinputs-react-wrapper';
+import '@progress/kendo-ui';
+import '@progress/kendo-theme-default/dist/all.css';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateTime: new Date()
+
+    }; //state
+    this.onchange = this.onchange.bind(this);
+  } // constructor
+
+  onchange(e) {
+    console.log(e.sender.value());
+    this.setState({
+      dateTime: e.sender.value()
+    }); //setState
+  } //onChange
+
+  render() {
+    return (
+      <div className="App">
+        <div className="date-picker">
+          <DatePicker
+            value={this.dateTime}
+            min={new Date(2017, 11, 1)}
+            max={new Date(2020, 1, 6)}
+            format={"yyyy/MM/dd"}
+            change={this.onchange}
+          />
+
+        </div>
+        Hi
+      </div>
+    ); //return
+  } //render
+} //class
 
 export default App;
